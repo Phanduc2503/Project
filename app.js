67234fb7422a4de04673630cb9b04bc5b254592c
Minh Đức <minhduc@Minhs-MBP.lan>
2026-06-28 22:19:42 +0700

    Resolve stash conflict and continue auth

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
 
 const express = require("express");
+
+const path = require("path");
+
 const app = express();
 
 const connectDB = require("./config/db");
+const authRoutes = require("./Route/authRoutes");
+
 
 connectDB();
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({
-  extended: true,
+    extended: true,
 }));
 
 app.use(express.static("public"));
@@ -18,5 +23,5 @@ app.use(express.static("public"));
 app.use(require("./routes/authRoutes"));
 
 app.listen(3000, () => {
-  console.log("Server running...");
+    console.log("Server running...");
 });
