ca92fe636f1630b280cca367092fe6a866655b5d
dungnt2743-cpu <dung.nt.2743@aptechlearning.edu.vn>
2026-07-02 22:32:42 +0700

    Added fake data on app.js and slight adjustments on index.ejs

--- app.js
+++ app.js
@@ -17,6 +17,91 @@ app.use(express.static("public"));
 
 app.use(require("./routes/authRoutes"));
 
+let favorites = [
+    { id: 1, name: "Golden Retriever" },
+    { id: 2, name: "Husky" },
+    { id: 3, name: "Corgi" }
+];
+
+let gallery = [
+    {
+        id: 1,
+        breed: "Golden Retriever",
+        image: "https://placedog.net/500"
+    },
+    {
+        id: 2,
+        breed: "Husky",
+        image: "https://placedog.net/501"
+    }
+];
+
+let preferences = {
+    homeSize: "",
+    lifestyle: "",
+    activityLevel: "",
+    climate: "",
+    familyType: ""
+};
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+app.get("/favorites", (req, res) => {
+    res.render("favorites", { favorites });
+});
+
+app.post("/favorites", (req, res) => {
+    favorites.push({
+        id: Date.now(),
+        name: req.body.name
+    });
+
+    res.redirect("/favorites");
+});
+
+app.post("/favorites/delete/:id", (req, res) => {
+    favorites = favorites.filter(
+        favorite => favorite.id != req.params.id
+    );
+
+    res.redirect("/favorites");
+});
+
+app.get("/gallery", (req, res) => {
+    res.render("gallery", { gallery });
+});
+
+app.post("/gallery", (req, res) => {
+    gallery.push({
+        id: Date.now(),
+        breed: req.body.breed,
+        image: req.body.image
+    });
+
+    res.redirect("/gallery");
+});
+
+app.post("/gallery/delete/:id", (req, res) => {
+    gallery = gallery.filter(
+        item => item.id != req.params.id
+    );
+
+    res.redirect("/gallery");
+});
+
+app.get("/preferences", (req, res) => {
+    res.render("preferences", { preferences });
+});
+
+app.post("/preferences", (req, res) => {
+    preferences = req.body;
+
+    res.redirect("/preferences");
+});
+
 app.listen(3000, () => {
   console.log("Server running...");
 });
